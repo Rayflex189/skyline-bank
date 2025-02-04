cecc2d512b0264098ca8c9d689c2a01cf4cd0186
Rayflex189 <roryisaiah66@gmail.com>
2025-02-03 18:10:19 -1100

    UPDATED

--- bank_site/bank_site/settings.py
+++ bank_site/bank_site/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_URL = 'static/'
+
 # Define STATIC_ROOT to collect static files for production and staging
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
