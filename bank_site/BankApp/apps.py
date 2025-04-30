f9891ab990f884601d26a815aa492b7c63e5afca
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-04-30 01:38:43 +0100

    Update apps.py
    Save

--- bank_site/BankApp/apps.py
+++ bank_site/BankApp/apps.py
@@ -1,20 +1,8 @@
 from django.apps import AppConfig
-from django.contrib.auth import get_user_model
-from django.conf import settings
-from django.db.utils import OperationalError, ProgrammingError
 
 class BankappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'BankApp'
 
     def ready(self):
-        try:
-            User = get_user_model()
-            if not User.objects.filter(email=settings.SUPERUSER_EMAIL).exists():
-                User.objects.create_superuser(
-                    email=settings.SUPERUSER_EMAIL,
-                    password=settings.SUPERUSER_PASSWORD
-                )
-        except (OperationalError, ProgrammingError):
-            # Avoid errors during migration or before DB is ready
-            pass
+        import BankApp.signals  # Register the signal
