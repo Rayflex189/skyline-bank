13f1ae87a5d3bba628b6aeea047d8b5d743c48a9
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-04-30 01:37:15 +0100

    Update signals.py
    Save

--- bank_site/BankApp/signals.py
+++ bank_site/BankApp/signals.py
@@ -1,6 +1,9 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_migrate
 from django.dispatch import receiver
 from .models import UserProfile, Transaction
+from django.contrib.auth import get_user_model
+from django.conf import settings
+
 
 @receiver(post_save, sender=UserProfile)
 def create_transaction_on_balance_update(sender, instance, **kwargs):
@@ -13,22 +16,32 @@ def create_transaction_on_balance_update(sender, instance, **kwargs):
         old_instance = UserProfile.objects.get(pk=instance.pk)
     except UserProfile.DoesNotExist:
         return
-    
+
     # Check if the balance has changed
     if old_instance.balance != instance.balance:
-        # Determine the amount to use in the transaction
         amount = instance.balance - old_instance.balance
         description = 'Credit' if amount > 0 else 'Debit'
-        
-        # Print statements for debugging
+
         print(f"Balance updated for user: {instance.user.username}")
         print(f"Old balance: {old_instance.balance}, New balance: {instance.balance}")
         print(f"Transaction type: {description}, Amount: {abs(amount)}")
-        
-        # Create a new Transaction
+
         Transaction.objects.create(
             user=instance.user,
-            amount=abs(amount),  # Use absolute value for amount
+            amount=abs(amount),
             balance_after=instance.balance,
             description=description
         )
+
+
+@receiver(post_migrate)
+def create_superuser_after_migrate(sender, **kwargs):
+    User = get_user_model()
+    email = settings.SUPERUSER_EMAIL
+    password = settings.SUPERUSER_PASSWORD
+
+    if not User.objects.filter(email=email).exists():
+        print(f"Creating superuser {email}")
+        User.objects.create_superuser(email=email, password=password)
+    else:
+        print(f"Superuser {email} already exists.")
