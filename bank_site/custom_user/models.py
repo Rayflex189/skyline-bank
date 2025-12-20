ee79438f59451943b72b4c2f667b0b032b7d64ba
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-12-20 05:21:56 +0000

    Updated login and registration views

--- bank_site/custom_user/models.py
+++ bank_site/custom_user/models.py
@@ -1,5 +1,10 @@
 from django_use_email_as_username.models import BaseUser, BaseUserManager
-
+from django.db import models
 
 class User(BaseUser):
+    is_email_verified = models.BooleanField(default=False)
+
     objects = BaseUserManager()
+
+    def __str__(self):
+        return self.email
